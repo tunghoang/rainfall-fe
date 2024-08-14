b7e5a07a248e80678fff6144da1fa336204ed9c8
admin <admin@localhost.localdomain>
2024-08-14 09:10:22 +0700

    merge by Tung

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/rainfall',
+  build: {
+    outDir: '../nginx-data/html/rainfall',
+    emptyOutDir: true, // also necessary
+  }
 })
